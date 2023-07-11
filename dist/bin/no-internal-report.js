19c55f29f69795fc7fc7d119dca71c8c81d3ce8a
Jake-Screen <Jake-Screen@users.noreply.github.com>
2023-07-11 15:57:23 -0400

    cleaning up script

--- dist/bin/no-internal-report.js
+++ dist/bin/no-internal-report.js
@@ -1,64 +1,39 @@
 #! /usr/bin/env node
-
 /*---------------------------------------------------------------------------------------------
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 "use strict"
 const path = require('path');
-const fs = require('fs');
+const assert = require('assert');
+const eslintBinPath = path.join(require.resolve("eslint"), "../../bin/eslint.js");
 
-function getNodeModulesDir(startPath) {
-  let currPath = startPath;
-  let baseName = path.basename(currPath);
-  while (baseName !== "node_modules") {
-    currPath = path.resolve(currPath, "..")
-    if (currPath === "/")
-      return undefined;
-    baseName = path.basename(currPath);
-  }
-  return currPath;
+const argCount = process.argv.length;
+try {
+  assert(argCount >= 3 && argCount <= 4);
+} catch(error) {
+  console.error("Incorrect number of arguments");
+  console.error("Usage: no-internal-report [tags: internal,alpha,beta,public] [FILE_GLOBS]");
+  console.error("The 'tag' argument may be one or more comma-separated api tag names")
+  process.exit(1);
 }
 
-const nodeModules = getNodeModulesDir(path.dirname(process.argv[1]));
-if (!nodeModules)
-  throw ("Could not find node_modules directory");
+const tags = argCount == 3 ? "" : process.argv[2].split(",");
+const custom = {"@itwin/no-internal":["error",{tag: tags }]};
 
-const distDir = path.join(nodeModules, "@itwin/eslint-plugin/dist")
-if (!fs.existsSync(distDir))
-  throw ("Could not find required dir: " + distDir);
-
-const configDir = path.join(nodeModules, "@itwin/eslint-plugin/dist/bin/eslint.config.js")
-if (!fs.existsSync(distDir))
-  throw ("Could not find required dir: " + configDir);
-
-let tags;
-if (process.argv.length > 3) {
-  tags = process.argv[2].toString();
-}
-
-
-// Run eslint with the appropriate configuration and formatter to get a report of the no-internal rule
-let args;
-if (tags) {
-  const custom = "{@itwin/no-internal:[error,{'tag':" + tags + "}]}";
-  args = [
-    "-f", path.join(distDir, "formatters/no-internal-summary.js"),
-    "-c", configDir,
-    "--rule", custom,
-    ...process.argv.slice(3)
-  ];
-} else
-  args = [
-    "-f", path.join(distDir, "formatters/no-internal-summary.js"),
-    "-c", configDir,
-    ...process.argv.slice(2)
-  ];
+const args = [
+  eslintBinPath,
+  "-f", require.resolve("../formatters/no-internal-summary.js"),
+  "--parser", "@typescript-eslint/parser",
+  "--parser-options", "{project:['tsconfig.json'],sourceType:'module'}",
+  "--rule", JSON.stringify(custom),
+  ...process.argv.slice(argCount - 1)
+];
 
 let results;
-const child = require('child_process');
 try {
-  results = child.execFileSync("eslint", args, { shell: true });
+  const child = require('child_process');
+  results = child.execFileSync("node", args);
 } catch (error) {
   results = error.stdout;
 }
