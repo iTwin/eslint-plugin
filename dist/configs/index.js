89a0dcf03d2f7e09f2e406431651c1b979436efd
paulius.valiunas <66480813+paulius-valiunas@users.noreply.github.com>
2024-01-17 13:56:18 +0200

    add no-node config

--- dist/configs/index.js
+++ dist/configs/index.js
@@ -2,10 +2,12 @@ const extensionExportsConfig = require("./extension-exports-config");
 const iTwinjsRecommendedConfig = require("./itwinjs-recommended");
 const jsdocConfig = require("./jsdoc");
 const uiConfig = require("./ui");
+const noNodeTypes = require("./no-node");
 
 module.exports = {
     extensionExportsConfig,
     iTwinjsRecommendedConfig,
     jsdocConfig,
-    uiConfig
+    uiConfig,
+    noNodeTypes,
 };
