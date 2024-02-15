8eeeab8ba3d8eb19c1520bb8d812ab3e671b01ec
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2024-02-15 10:29:29 -0500

    Re-enable no-unsafe-enum-comparison

--- dist/configs/itwinjs-recommended.js
+++ dist/configs/itwinjs-recommended.js
@@ -177,7 +177,7 @@ module.exports =
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-enum-comparison": "off",
+    "@typescript-eslint/no-unsafe-enum-comparison": "error",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unused-vars": [
