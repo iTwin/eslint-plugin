3d93bff4a43d53105176314a1877aabaeacf6eb3
Erin Ingram <47707444+eringram@users.noreply.github.com>
2024-07-23 09:42:53 -0400

    Add new tests and update test fixtures

--- tests/no-internal.js
+++ tests/no-internal.js
@@ -36,16 +36,16 @@ ruleTester.run(
   supportSkippedAndOnlyInTests({
     valid: [
       {
-        // local import
+        name: "Internal API from a local file",
         code: `import * as Local from "./local-internal"; Local.internal(); new Local.Internal();`
       },
       {
-        // local import and package name is specified
+        name: "Internal API from a local file, and the package name is specified in options",
         code: `import * as Local from "./local-internal"; Local.internal(); new Local.Internal();`,
         options: [{ "checkedPackagePatterns": ["workspace-pkg-1"] }],
       },
       {
-        // not a bentley/itwin scope
+        name: "Internal API from an installed dependency that isn't in the @bentley/@itwin scope",
         code: dedent`
           import { internal, public, Internal, Public } from "test-pkg-1";
           public();
@@ -57,7 +57,7 @@ ruleTester.run(
         `
       },
       {
-        // other package in workspace allowed
+        name: "Internal API from another package in the same workspace",
         code: dedent`
           import { internal, Public } from "workspace-pkg-2";
           internal();
@@ -65,7 +65,7 @@ ruleTester.run(
         `
       },
       {
-        // other package in workspace not allowed, but name not specified
+        name: "Internal API from another package in the same workspace, but the package name is not specified",
         code: dedent`
           import { internal, Public } from "workspace-pkg-2";
           internal();
@@ -75,16 +75,31 @@ ruleTester.run(
           "dontAllowWorkspaceInternal": true,
         }],
       },
+      {
+        name: "Internal API in an itwinjs-core repository package from another itwinjs-core repository package",
+        code: dedent`
+          import { internal, public, Internal, Public } from "@itwin/test-pkg-2";
+          internal();
+          public();
+          new Internal();
+          new Internal().publicMethod();
+          new Public();
+          new Public().internalMethod();
+        `,
+        filename: path.join(fixtureDir, "node_modules", "@itwin", "test-pkg-3", "index.d.ts"),
+      },
     ],
     invalid: [
       {
-        // itwin scope
+        name: "Internal API from an installed dependency in the @bentley/@itwin scope",
         code: dedent`
-          import { internal, public, Internal, Public } from "@itwin/test-pkg-2";
+          import { internal, public, Internal, Public, alpha, beta } from "@itwin/test-pkg-2";
           public();
           internal();
           internal();
           internal();
+          alpha();
+          beta();
           new Internal();
           new Internal().publicMethod();
           new Public();
@@ -94,13 +109,14 @@ ruleTester.run(
           { message: 'function "internal" is internal.' },
           { message: 'function "internal" is internal.' },
           { message: 'function "internal" is internal.' },
+          { message: 'function "alpha" is alpha.'},
           { message: 'class "Internal" is internal.' },
           { message: 'class "Internal" is internal.' },
-          { message: 'method "Public.internalMethod" is internal.' }
-        ]
+          { message: 'method "Public.internalMethod" is internal.' },
+        ],
       },
       {
-        // package name is specified to disallow @internal
+        name: "Internal API from an installed dependency, and the package name is specified in options",
         code: dedent`
           import { internal, public, Internal, Public } from "test-pkg-1";
           public();
@@ -120,10 +136,24 @@ ruleTester.run(
           { message: 'class "Internal" is internal.' },
           { message: 'class "Internal" is internal.' },
           { message: 'method "Public.internalMethod" is internal.' },
-        ]
+        ],
       },
       {
-        // other package in workspace & package name is specified to disallow @internal
+        name: "Alpha and beta APIs, and those tags are specified in options",
+        code: dedent`
+          import { internal, alpha, beta } from "@itwin/test-pkg-2";
+          internal();
+          alpha();
+          beta();
+        `,
+        options: [{ "tag": ["alpha", "beta"] }],
+        errors: [
+          { message: 'function "alpha" is alpha.'},
+          { message: 'function "beta" is beta.'},
+        ],
+      },
+      {
+        name: "Internal API from another package in the same workspace, and the package name is specified in options",
         code: dedent`
           import { internal, Public } from "workspace-pkg-2";
           internal();
@@ -141,7 +171,7 @@ ruleTester.run(
         ]
       },
       {
-        // other package in workspace & itwin/bentley scope
+        name: "Internal API from another package in the same workspace in the @itwin/@bentley scope",
         code: dedent`
           import { internal, Public } from "@bentley/workspace-pkg-3";
           internal();
