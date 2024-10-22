10c3eb11e6a4b8470fc44b0ae4b7fe6e2938da15
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2024-10-22 16:28:24 -0400

    Add meta field to plugin definition

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,10 @@
 const rules = require("./rules");
+const version = require("../package.json").version;
 
 module.exports = {
+  meta: {
+    name: "@itwin/eslint-plugin",
+    version,
+  },
   rules,
 };
