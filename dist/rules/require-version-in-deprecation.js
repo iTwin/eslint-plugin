7afeae2d86f7cf15d09ba2899f344d8d6442bf43
Paulius Valiūnas <66480813+paulius-valiunas@users.noreply.github.com>
2025-05-28 13:52:44 +0300

    update require-version-in-deprecation for new deprecation policy (#102)
    * update require-version-in-deprecation for new deprecation policy
    
    * use corepack in workflows
    
    * write tests for default settings, fix bugs
    
    * add more test cases
    
    * require complete versions
    
    * report all problems in one go
    
    * Change files

--- dist/rules/require-version-in-deprecation.js
+++ dist/rules/require-version-in-deprecation.js
@@ -1,33 +1,187 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * @import { Rule, Scope, AST } from "eslint"
+ */
 
 //Custom rule that enforces version and description for @deprecated
 
 "use strict";
 
+const { DateTime } = require("luxon");
+
+const regexParts = {
+  expired: "might be removed in next major version",
+  notUntil: "will not be removed until",
+};
+
+const deprecatedCommentRegex = new RegExp(
+  [
+    /@deprecated(?=[\W])(?: in (?<version>\d+(?:\.(?:\d|x)+){1,2}))?/.source,
+    `(?: - (?<when>(?:${regexParts.expired})|(?:${regexParts.notUntil} (?<date>[0-9]{4}(?:-[0-9]{2}){2}))))?`,
+    /(?:(?<separator>\.[^\S\r\n])?(?:[^\S\r\n]*)?(?<description>(?=[\S]).{5,}))?/.source,
+  ].join(""),
+  "gdu",
+);
+
+const validDescriptionRegex = /^[\w\]`]/;
+
+const messages = {
+  noDescription: "@deprecated must be followed by deprecation reason and/or alternative API usage required",
+  doubleDeprecation: "Multiple @deprecated notes found in the same comment",
+  oldDate: `Expired deprecation date was found and should be replaced with "${regexParts.expired}"`,
+  noDate: `@deprecated should be followed by either "${regexParts.notUntil} {YYYY-MM-dd}" or "${regexParts.expired}"`,
+  noVersion: `@deprecated should be followed by "in {major}.{minor}`,
+  noSeparator:
+    "Deprecation reason should be separated from any preceding text by `. ` ONLY if there is a version or other information before the description",
+  badDescription: `Deprecation reason should match regex ${validDescriptionRegex.source.replace("\\\\", "\\")}`,
+  badVersion: "Version numbers should be complete - no 'x' allowed",
+};
+
+/** @type {typeof messages} */
+const messageIds = Object.keys(messages).reduce((obj, key) => {
+  obj[key] = key;
+  return obj;
+}, {});
+
+/** @param {string} str */
+function firstUpper(str) {
+  return `${str[0].toUpperCase()}${str.substring(1)}`;
+}
+
+/**
+ * @param {Rule.RuleFixer} fixer
+ * @param {AST.Program["comments"][0]} comment
+ * @param {string} newText
+ */
+function wrapComment(fixer, comment, newText) {
+  // @ts-expect-error -- comments are not considered nodes but this still works
+  return fixer.replaceText(comment, comment.type === "Block" ? `/*${newText}*/` : `//${newText}`);
+}
+
+/** @type {Rule.RuleModule & {messageIds: typeof messageIds}} */
 module.exports = {
   meta: {
-    messages: {
-      requireVersionAndSentence: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required"
-    }
+    type: "problem",
+    messages,
+    fixable: "whitespace",
+    schema: [
+      {
+        type: "object",
+        properties: {
+          removeOldDates: {
+            type: "boolean",
+          },
+          addVersion: {
+            type: "string",
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
   },
   create(context) {
     return {
-      Program(node) {
-        let match;
-        for (const comment of node.comments) {
-          if (match = /@deprecated(?<in> in \d+\.(\d|x)+[.,\s](?<sentence>.+))?/.exec(comment.value)) {
-            if ((match?.groups?.in) && (match?.groups?.sentence && match?.groups?.sentence.replace(/\s/g, '').length > 5)) {
-              continue;
+      Program(program) {
+        let regexMatch;
+        for (const comment of program.comments ?? []) {
+          let found = 0;
+          /** @type {Rule.Node} */
+          // @ts-expect-error -- comments are not considered nodes but this still works
+          const node = comment;
+          /** @type {Scope.Scope["block"]} */
+          while ((regexMatch = deprecatedCommentRegex.exec(comment.value))) {
+            const match = regexMatch;
+            if (found === 1) {
+              context.report({
+                node,
+                messageId: messageIds.doubleDeprecation,
+              });
             }
-            else {
-              context.report({ node: comment, messageId: "requireVersionAndSentence" });
+            found++;
+            if (!match.groups?.description) {
+              context.report({
+                node,
+                messageId: messageIds.noDescription,
+              });
+              continue; // no point in applying any fixes after this - the description will have to be added manually anyway.
             }
+
+            let noVersion = false;
+            let addDate = false;
+            let oldDate = false;
+            let addSeparator = false;
+            let removeSeparator = false;
+            let badDescription = false;
+            if (!match.groups?.version) {
+              // the version is missing
+              if (context.options[0]?.addVersion || match.groups?.when) {
+                // either the options require having a version, or the deprecation date is already there, in both cases the version should also be there.
+                noVersion = true;
+              }
+            }
+
+            if (match.groups?.version?.includes("x")) {
+              context.report({
+                node,
+                messageId: messageIds.badVersion,
+              });
+            }
+
+            const now = DateTime.now();
+            const currentDate = now.toFormat("yyyy-MM-dd");
+            const targetDate = now.plus({ year: 1 }).toFormat("yyyy-MM-dd");
+
+            if (context.options[0]?.addVersion && !match.groups?.when) {
+              // add date
+              addDate = true;
+            }
+
+            if (context.options[0]?.removeOldDates && match.groups?.date && match.groups.date < currentDate) {
+              // remove old date
+              oldDate = true;
+            }
+
+            if (match.groups?.description && !validDescriptionRegex.test(match.groups.description)) badDescription = true;
+
+            if ((match.groups?.version || match.groups?.when) && !match.groups?.separator && match.groups?.description) addSeparator = true;
+            else if (!(match.groups?.version || match.groups?.when) && match.groups?.separator && match.groups?.description) removeSeparator = true;
+
+            const oldDescription = match.groups?.description?.trimStart() ?? "";
+            const newDescription = firstUpper(oldDescription.replace(/^-\s*/, ""));
+            const newComment =
+              comment.value.substring(0, match.index) +
+              "@deprecated" +
+              (noVersion && context.options[0]?.addVersion
+                ? ` in ${context.options[0].addVersion}`
+                : match.groups?.version
+                ? ` in ${match.groups.version}`
+                : "") +
+              (addDate
+                ? ` - ${regexParts.notUntil} ${targetDate}`
+                : oldDate
+                ? ` - ${regexParts.expired}`
+                : match.groups?.when
+                ? ` - ${match.groups.when}`
+                : "") +
+              (addDate || oldDate || match.groups?.when || noVersion || match.groups?.version ? "." : "") +
+              ` ${newDescription}` +
+              comment.value.substring(match.index + match[0].length);
+
+            /** @param {Rule.RuleFixer} fixer */
+            const fix = newComment !== comment.value ? (fixer) => wrapComment(fixer, comment, newComment) : undefined;
+            if (noVersion) context.report({ node, messageId: messageIds.noVersion, fix });
+            if (addDate) context.report({ node, messageId: messageIds.noDate, fix });
+            if (oldDate) context.report({ node, messageId: messageIds.oldDate, fix });
+            if (addSeparator || removeSeparator) context.report({ node, messageId: messageIds.noSeparator, fix });
+            if (badDescription) context.report({ node, messageId: messageIds.badDescription, fix });
           }
         }
-      }
-    }
-  }
-}
+      },
+    };
+  },
+  messageIds,
+};
--- tests/require-version-in-deprecation.js
+++ tests/require-version-in-deprecation.js
@@ -1,16 +1,16 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 
 "use strict";
 
-const path = require("path");
+const { DateTime } = require("luxon");
+
 const ESLintTester = require("eslint").RuleTester;
 const BentleyESLintPlugin = require("../dist");
-const RequireVersionInDeprecationESLintRule =
-  BentleyESLintPlugin.rules["require-version-in-deprecation"];
-const { supportSkippedAndOnlyInTests } = require("./test-utils");
+const rule = BentleyESLintPlugin.rules["require-version-in-deprecation"];
+const { supportSkippedAndOnlyInTests, wrapMessageIds: messageIds } = require("./test-utils");
 
 const ruleTester = new ESLintTester({
   languageOptions: {
@@ -23,52 +23,143 @@ const ruleTester = new ESLintTester({
 
 ruleTester.run(
   "require-version-in-deprecation",
-  RequireVersionInDeprecationESLintRule,
+  rule,
   supportSkippedAndOnlyInTests({
     valid: [
       {
         code: `/**
       * @beta
-      * @deprecated in 3.x. Use XYZ instead, see https://www.google.com/ for more details.
-      */`
+      * @deprecated in 3.0. Use XYZ instead, see https://www.google.com/ for more details.
+      */`,
       },
       {
         code: `/**
-        * @deprecated in 3.x, use XYZ instead
+        * @deprecated in 3.0. Use XYZ instead
         * @beta
-        */`
+        */`,
       },
+      { code: `// @deprecated in 3.6. Use XYZ instead.` },
+      { code: `/* @deprecated in 12.0. Use xyz instead. */` },
       {
-        code: `// @deprecated in 3.6 Use XYZ instead.`
-      },
-      {
-        code: `/* @deprecated in 12.x. Use xyz instead. */`
-      },
-      {
-        code: `// @deprecated in 2.19, use xyz instead
-        function canWeUseFunctions() {}`
+        code: `// @deprecated in 2.19.  Use xyz instead
+        function canWeUseFunctions() {}`,
       },
       {
         code: `// @deprecated in 3.6. Use xyz instead.
-        class canWeUseAClass {};`
+        class canWeUseAClass {};`,
       },
       {
-        code: `/* @deprecated in 2.x, Use XYZ instead */
-        let canWeUseArrowFunction = () => {};`
+        code: `/* @deprecated in 2.0. Use XYZ instead */
+        let canWeUseArrowFunction = () => {};`,
+      },
+      {
+        code: `/**
+        * @deprecated in 3.0. Please use XYZ.
+        */
+        export interface canWeUseInterface {}`,
+      },
+      {
+        code: `/* @deprecated in 2.0. Use xyz instead. */
+        namespace canWeUseNamespaces {}`,
+      },
+      {
+        code: `// @deprecated in 3.6. Use XYZ instead.
+        export enum canWeUseEnum {}`,
+      },
+      {
+        code: `
+        interface BackendHubAccess {
+          /**
+           * download a v1 checkpoint
+           * @deprecated in 3.0. Here's a description.
+           * @internal
+           */
+          downloadV1Checkpoint: (arg: CheckpointArg) => Promise<ChangesetIndexAndId>;
+        }
+        `,
+      },
+      { code: `// @deprecated in 2.0. Use [[InternalDocRef]] instead` },
+      { code: `/** @deprecated in 3.0. Use [ExternalDocRef]($package) instead */` },
+      { code: `/** @deprecated in 2.1 - will not be removed until 2022-01-01. Use [[methodB]] instead */` },
+      { code: `/** @deprecated Use [[methodB]] instead */` },
+      { code: `/** @deprecated in 3.2. Use [[methodB]] instead */` },
+      { code: `/** @deprecated in 3.2. use [[methodB]] instead */` },
+    ],
+    invalid: [
+      {
+        code: `/**
+        * @beta
+        * @deprecated
+        */`,
+        errors: messageIds([rule.messageIds.noDescription]),
+      },
+      {
+        code: `/**
+        * @deprecated Use [[A]] instead
+        * @deprecated Use [[B]] instead
+        */`,
+        errors: messageIds([rule.messageIds.doubleDeprecation]),
+      },
+      {
+        code: `// @deprecated in 3.x.x.x. Use [[A]]`,
+        // The error can be a bit misleading. Since versions can only have 3 parts, the rule assumes that the last `.x` is part of the description.
+        // However, it does give the developer a hint about what exactly is going wrong by splitting the version and inserting a space.
+        errors: messageIds([rule.messageIds.noSeparator, rule.messageIds.badDescription]),
+        output: "// @deprecated in 3.x.x. .x. Use [[A]]",
+        // This test is there only to illustrate some quirky edge cases the rule might run into and what we should expect from its behavior.
+        // There's no need to guarantee the exact same behavior when updating the rule.
+        skip: true,
+      },
+      {
+        code: `/** @deprecated. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noSeparator]),
+        output: `/** @deprecated Use [[methodB]] instead */`,
+      },
+      {
+        code: `/** @deprecated in 3.2 use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noSeparator]),
+        output: `/** @deprecated in 3.2. Use [[methodB]] instead */`,
+      },
+      {
+        code: `/** @deprecated in 3.2 - use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noSeparator, rule.messageIds.badDescription]),
+        output: `/** @deprecated in 3.2. Use [[methodB]] instead */`,
+      },
+      {
+        code: `/** @deprecated - will not be removed until 2022-01-01. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noVersion]),
+      },
+      {
+        code: `/**
+      * @beta
+      * @deprecated in 3.x. Use XYZ instead, see https://www.google.com/ for more details.
+      */`,
+        errors: messageIds([rule.messageIds.badVersion]),
+      },
+      {
+        code: `/**
+        * @deprecated in 3.x. Use XYZ instead
+        * @beta
+        */`,
+        errors: messageIds([rule.messageIds.badVersion]),
+      },
+      { code: `/* @deprecated in 12.x. Use xyz instead. */`, errors: messageIds([rule.messageIds.badVersion]) },
+      {
+        code: `/* @deprecated in 2.x. Use XYZ instead */
+        let canWeUseArrowFunction = () => {};`,
+        errors: messageIds([rule.messageIds.badVersion]),
       },
       {
         code: `/**
         * @deprecated in 3.x. Please use XYZ.
         */
-        export interface canWeUseInterface {}`
+        export interface canWeUseInterface {}`,
+        errors: messageIds([rule.messageIds.badVersion]),
       },
       {
         code: `/* @deprecated in 2.x. Use xyz instead. */
-        namespace canWeUseNamespaces {}`
-      },
-      {
-        code: `// @deprecated in 3.6. Use XYZ instead.
-        export enum canWeUseEnum {}`
+        namespace canWeUseNamespaces {}`,
+        errors: messageIds([rule.messageIds.badVersion]),
       },
       {
         code: `
@@ -81,83 +172,166 @@ ruleTester.run(
           downloadV1Checkpoint: (arg: CheckpointArg) => Promise<ChangesetIndexAndId>;
         }
         `,
-      },
-      {
-        code: `// @deprecated in 2.x. Use [[InternalDocRef]] instead`
-      },
-      {
-        code: `/** @deprecated in 3.x. Use [ExternalDocRef]($package) instead */`
+        errors: messageIds([rule.messageIds.badVersion]),
       },
     ],
+  }),
+);
+
+ruleTester.run(
+  "require-version-in-deprecation with removeOldDates",
+  rule,
+  supportSkippedAndOnlyInTests({
+    valid: [
+      { code: `/** @deprecated in 2.1 - will not be removed until 3022-01-01. Use [[methodB]] instead */` },
+      { code: `/** @deprecated Use [[methodB]] instead */` },
+      { code: `/** @deprecated in 3.2. Use [[methodB]] instead */` },
+      { code: `/** @deprecated in 2.1 - might be removed in next major version. Use [[methodB]] instead */` },
+    ].map((testCase) => ({
+      ...testCase,
+      options: [{ removeOldDates: true }],
+    })),
     invalid: [
       {
         code: `/**
         * @beta
         * @deprecated
         */`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        errors: messageIds([rule.messageIds.noDescription]),
       },
       {
-        code: `// @deprecated`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/**
+        * @deprecated Use [[A]] instead
+        * @deprecated Use [[B]] instead
+        */`,
+        errors: messageIds([rule.messageIds.doubleDeprecation]),
       },
       {
-        code: `// @deprecated in 3.x.`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noSeparator]),
+        output: `/** @deprecated Use [[methodB]] instead */`,
       },
       {
-        code: `/* @deprecated */`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated in 3.2 use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noSeparator]),
+        output: `/** @deprecated in 3.2. Use [[methodB]] instead */`,
       },
       {
-        code: `/* @deprecated in 2.6.*/
-        function shouldFailSinceNoSentence() {}`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated in 3.2 - use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noSeparator, rule.messageIds.badDescription]),
+        output: `/** @deprecated in 3.2. Use [[methodB]] instead */`,
       },
       {
-        code: `// @deprecated in 3.x
-        let shouldFailSinceNoSentenceGiven = () => {};`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated in 2.1 - will not be removed until 2022-01-01. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.oldDate]),
+        output: `/** @deprecated in 2.1 - might be removed in next major version. Use [[methodB]] instead */`,
       },
       {
-        code: `// @deprecated
-        class shouldFailSinceNoVersionOrSentenceGiven {};`,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated - will not be removed until 2022-01-01. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noVersion, rule.messageIds.oldDate]),
+        output: `/** @deprecated - might be removed in next major version. Use [[methodB]] instead */`,
       },
       {
-        code: `/** @deprecated in 3.6.
-        * I am giving the description here.
-        */
-         `,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated - might be removed in next major version. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noVersion]),
       },
       {
-        code: `
-          /** @deprecated in 3.6. Ok.*/
-          function descriptionTooShort() {}
-        `,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/** @deprecated - will not be removed until 3022-01-01. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noVersion]),
+      },
+    ].map((testCase) => ({ ...testCase, options: [{ removeOldDates: true }] })),
+  }),
+);
+
+const targetDate = DateTime.now().plus({ year: 1 }).toFormat("yyyy-MM-dd");
+
+ruleTester.run(
+  "require-version-in-deprecation with addVersion",
+  rule,
+  supportSkippedAndOnlyInTests({
+    valid: [
+      { code: `/** @deprecated in 2.1 - will not be removed until 3022-01-01. Use [[methodB]] instead */` },
+      { code: `/** @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[methodB]] instead */` },
+      { code: `/** @deprecated in 3.2 - will not be removed until ${targetDate}. Use [[methodB]] instead */` },
+      {
+        code: `/** 
+                * @deprecated in 3.0 - will not be removed until ${targetDate}. Use [[B]]
+                * @public
+                */`,
+      },
+    ].map((testCase) => ({
+      ...testCase,
+      options: [{ addVersion: "10.0" }],
+    })),
+    invalid: [
+      {
+        code: `/**
+        * @beta
+        * @deprecated
+        */`,
+        errors: messageIds([rule.messageIds.noDescription]),
       },
       {
-        code: `
-          /** @deprecated in 3.6.                                                        */
-          function whitespaceIsNotADescription() {}
-        `,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/**
+        * @deprecated Use [[A]] instead
+        * @deprecated Use [[B]] instead
+        */`,
+        errors: messageIds([
+          rule.messageIds.noVersion,
+          rule.messageIds.noDate,
+          rule.messageIds.doubleDeprecation,
+          rule.messageIds.noVersion,
+          rule.messageIds.noDate,
+        ]),
+        output: `/**
+        * @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[A]] instead
+        * @deprecated Use [[B]] instead
+        */`,
       },
       {
-        code: `
-        interface BackendHubAccess {
-          /**
-           * download a v1 checkpoint
-           * @deprecated in 3.x.
-           * @internal
-           */
-          downloadV1Checkpoint: (arg: CheckpointArg) => Promise<ChangesetIndexAndId>;
-        }
-        `,
-        errors: [{ messageId: "requireVersionAndSentence" }],
+        code: `/**
+        * @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[A]] instead
+        * @deprecated Use [[B]] instead
+        */`,
+        errors: messageIds([rule.messageIds.doubleDeprecation, rule.messageIds.noVersion, rule.messageIds.noDate]),
+        output: `/**
+        * @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[A]] instead
+        * @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[B]] instead
+        */`,
       },
-    ],
-  })
+      {
+        code: `/**
+        * @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[A]] instead
+        * @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[B]] instead
+        */`,
+        errors: messageIds([rule.messageIds.doubleDeprecation]),
+      },
+      {
+        code: `/** @deprecated. Use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noVersion, rule.messageIds.noDate, rule.messageIds.noSeparator]),
+        output: `/** @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[methodB]] instead */`,
+      },
+      {
+        code: `/** @deprecated in 10.0 use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noDate, rule.messageIds.noSeparator]),
+        output: `/** @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[methodB]] instead */`,
+      },
+      {
+        code: `/** @deprecated in 10.0 - use [[methodB]] instead */`,
+        errors: messageIds([rule.messageIds.noDate, rule.messageIds.noSeparator, rule.messageIds.badDescription]),
+        output: `/** @deprecated in 10.0 - will not be removed until ${targetDate}. Use [[methodB]] instead */`,
+      },
+      {
+        code: `/** 
+                * @deprecated in 3.0 - use [[B]]
+                * @public
+                */`,
+        errors: messageIds([rule.messageIds.noDate, rule.messageIds.noSeparator, rule.messageIds.badDescription]),
+        output: `/** 
+                * @deprecated in 3.0 - will not be removed until ${targetDate}. Use [[B]]
+                * @public
+                */`,
+      },
+    ].map((testCase) => ({ ...testCase, options: [{ addVersion: "10.0" }] })),
+  }),
 );
